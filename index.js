8b57ffd6da89d283a868123be9f802058f75d356
Rakesh Shivanoor <rakeshshivanoor45@gmail.com>
2023-03-25 19:36:19 +0530

    modified index.js package.json

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import bodyParser from "body-parser";
 import Connection from "./database/db.js";
 import route from "./routes/route.js";
 import dotenv from "dotenv";
-import path from "path";
 
 dotenv.config();
 const app = express();
@@ -14,27 +13,9 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", route);
 
-// ----------Deployment
-
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname1, "/client/build")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
-	});
-} else {
-	app.listen(PORT, () => {
-		console.log("server Successfully running on port " + PORT);
-	});
-}
-
-
-//----Deployment
-
 Connection();
 
-const PORT = 8000;
+const PORT = process.env.PORT | 8000;
 
 app.listen(PORT, () => {
 	console.log("server Successfully running on port " + PORT);
